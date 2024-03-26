af840ccc06da1f610d6dfa6b46bbae25702cc3c4
bnorthern42 <54911506+bnorthern42@users.noreply.github.com>
2024-03-26 13:19:19 -0500

    adeded new kingdom administration page

--- src/main/java/net/northern/wurmmaps/Controllers/HomeController.java
+++ src/main/java/net/northern/wurmmaps/Controllers/HomeController.java
@@ -1,18 +1,24 @@
 package net.northern.wurmmaps.Controllers;
 
+import net.northern.wurmmaps.Services.KingdomsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Arrays;
+import java.util.List;
+
 
 @Controller
 public class HomeController {
 
+
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
 
          /*model.addAllAttributes("features", Arrays.asList(
                 "bo",
@@ -20,8 +26,9 @@ public class HomeController {
                 "idk"));*/
         return "index";
     }
+
     @GetMapping("/map")
-    public String map(Model model){
+    public String map(Model model) {
 
          /*model.addAllAttributes("features", Arrays.asList(
                 "bo",
@@ -32,7 +39,9 @@ public class HomeController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
-    public String admin(){
+    public String admin() {
         return "Hello Admin";
     }
+
+
 }
